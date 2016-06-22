1549ae394460c73a02f5c78a4b9fb8d37f45f18b
jchiou <jessica.chiou@gmail.com>
2016-06-21 17:07:04 -0700

    Saving before using es6 within script
    friendlist loses highlight on refresh and on room click
    
    characters need to be unescaped, as they are still currently encoded

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -21,7 +21,7 @@ $(document).ready(function() {
     // });
   };
 
-  app.send = function(message) {
+  app.send = function(val) {
     // var name = window.location.search.slice(10);
     // var text = $('.input').val();
     // var roomname = $('#roomSelect :selected').text(); //TODO
@@ -35,7 +35,20 @@ $(document).ready(function() {
       contentType: 'application/json',
       success: function (data) {
         console.log('chatterbox: Message sent');
-        $('#chats').append('<div class="chat">' + '<a href="#" class="username">' + message.username + '</a>' + ':\n' + message.text + '</div>');
+        //roomname not here
+
+        var $user = $('<a href="#" class="username"></a>');
+        var $message = $('<div class="chat"></div>');
+
+        $user.text('@' + val.username);
+        $user.attr('data-user', _.escape(val.username));
+        $user.prependTo($message);
+
+        $message.append(': ' + _.escape(val.text));
+        $message.attr('data-room', _.escape(val.roomname));
+
+        $('#chats').append($message);
+
       },
       error: function (data) {
         // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -51,23 +64,9 @@ $(document).ready(function() {
     message.text = $('.input').val();
     message.roomname = $('#roomSelect :selected').text();
 
-    $.ajax({
-    // This is the url you should use to communicate with the parse API server.
-      url: 'https://api.parse.com/1/classes/messages',
-      type: 'POST',
-      data: JSON.stringify(message),
-      dataType: 'json',
-      contentType: 'application/json',
-      success: function (data) {
-        alert('Message sent');
-        $('#chats').append('<div class="chat">' + '<a href="#" class="username">' + message.username + '</a>' + ':\n' + message.text + '</div>');
-      },
-      error: function (data) {
-        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-        console.error('chatterbox: Failed to send message', data);
-      }
-    });
-
+    app.send(message);
+    app.clearMessages();  
+    app.fetch();
   });
 
 
@@ -81,27 +80,22 @@ $(document).ready(function() {
         success: function(data) {
           $.each(data.results, function(index, val) {
 
-            console.log(val.text, val.username, val.roomname);
+            // console.log(val.text, val.username, val.roomname);
 
 
             var $user = $('<a href="#" class="username"></a>');
             var $message = $('<div class="chat"></div>');
 
             $user.text('@' + val.username);
-            $user.attr('data-user', val.username);
+            $user.attr('data-user', _.escape(val.username));
+            $message.text(': ' + val.text);
+
             $user.prependTo($message);
 
-            $message.append(': ' + val.text);
-            $message.attr('data-room', val.roomname);
+            $message.attr('data-room', _.escape(val.roomname));
 
             // update dropdown menu with chatrooms
-            if (chatRoomList.indexOf(val.roomname) === -1) {
-              chatRoomList.push(val.roomname);
-              $('#roomSelect').append('<option value=' + val.roomname + '>' + val.roomname + '</option>');
-            }
-            
-            
-
+            app.addRoom(val.roomname);
 
             $('#chats').append($message);
           });
@@ -118,7 +112,7 @@ $(document).ready(function() {
   app.init();
 
 
-  app.addMessage = function(message) {
+  app.addMessage = function(val) {
 
 
     var $user = $('<a href="#" class="username"></a>');
@@ -142,9 +136,21 @@ $(document).ready(function() {
     $('#chats').empty();
   };
 
-  app.addRoom = function(room) {
-    $('#roomSelect').append('<option value="value2">' + room + '</option>');
+  app.addRoom = function(val) {
+    var val = val || $('.newroom').val();
+
+    if (chatRoomList.indexOf(val) === -1) {
+      chatRoomList.push(val);
+      $('#roomSelect').append('<option value=' + val + '>' + val + '</option>');
+    }
   };
+
+  $('.addRoom').click(function() {
+    console.log('clicked');
+    app.addRoom();
+  });
+
+
 
   app.addFriend = function(message) {
     $('.friendsList').append(message.username);
@@ -153,55 +159,36 @@ $(document).ready(function() {
   $('#chats').on('click', 'a', function() {
     // $('.friendsList').append($(this).attr('data-user'));
     var clickedFriend = $(this).attr('data-user');
-    console.log(clickedFriend);
+    // console.log(clickedFriend);
     if (friendlist.indexOf(clickedFriend) === -1) {
-      $('[data-user="' + clickedFriend + '"]').addClass('friend');
+      $('[data-user="' + _.escape(clickedFriend) + '"]').addClass('friend');
       friendlist.push(clickedFriend);
       $('.friendsList').append('<p class="friend">' + clickedFriend + '</p>');
     }
   });
 
+
+
   //refresh to see new messages
-  $('.refresh').click(function() {
+  $('.refresh').click( () => {
     app.clearMessages();
+    app.fetch();
+    $('#currentroom').text('all chat rooms');
+  });
+
+
+  $('.dropdown').click( () => {
     app.fetch();
   });
 
-  app.filterRooms = function(tarRoom) {
-    app.clearMessages();
-    var rooms = app.fetch();
-    console.log(rooms);
-    for (var key in rooms) {
-      // console.log(key);
-      if (key === 'roomname' && key === tarRoom) {
-        app.addMessage(key);
-      }
-    }
-  };
-  // setInterval(function() { app.fetch(); }, 1000);
-});
-
-$(document).ready(function() {
-
-  $('.dropdown').change(function() {
-    var roomOptions = [];
-    alert($(this).val());
-    app.fetch();
-    //find a way to push the room names into roomoption then use the below code to 
-    //apend to select dropdown
-    //http://stackoverflow.com/questions/317095/how-do-i-add-options-to-a-dropdownlist-using-jquery
-    // var mySelect = $('#roomSelect');
-    // $.each(roomOptions, function(val, text) {
-    //   mySelect.append($('<option></option>').val(val).html(text));
-    // });
-    // app.clearMessages();
-    //when a room is selected, it will be :selected
-    //use that attr to get the message
-    //
+  $('.dropdown').change( () => {
+    // app.fetch();
 
     var chatroomSelected = $(this).val();
     var msg = document.querySelectorAll('[data-room="' + chatroomSelected + '"]');
-    console.log(msg);
+    $('#currentroom').text(_.unescape(chatroomSelected));
+    // console.log(chatroomSelected);
+    // console.log(app.clearMessages());
     app.clearMessages();
   // for each chatNode this function searches the data attribute "data-room"
     $.each(msg, function(index, chatNode) {
@@ -209,26 +196,11 @@ $(document).ready(function() {
       var text = chatNode.innerText.slice(user.length + 3);
       var obj = {username: user, text: text, roomname: chatroomSelected};
       // console.log(obj);
+
       app.addMessage(obj);
-      // console.log(chatroomSelected);
-      // if (chatNode.attributes[1].value === chatroomSelected) {
-      //   console.log(chatNode.attributes[1].value);
-        //This line of code goes with the above commented out
-        // roomOptions.push(chatNode.attributes[1].value);
     });
   });
 
-
-
 });
-// var myOptions = {
-//   val1: 'text1',
-//   val2: 'text2'
-// };
-// var mySelect = $('#roomSelect');
-// $.each(myOptions, function(val, text) {
-//   mySelect.append($('<option></option>').val(val).html(text));
-// });
 
-//https://css-tricks.com/dynamic-dropdowns/
-//http://stackoverflow.com/questions/20868180/how-to-populate-dropdown-dynamically
+
